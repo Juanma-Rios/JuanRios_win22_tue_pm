79443bb4029ff7f6bc597408338ab0e3c6f20ec6
juan1 <riosmore@shernet.sheridancollege.ca>
2022-01-18 13:43:03 -0500

    Checkpoint 1 - added the introduction to the toc and began writing the introduction part

--- src/student/Student.java
+++ src/student/Student.java
@@ -5,31 +5,37 @@
  */
 package student;
 
-/**for pull
- *this is commment added
+/** for pull
+ * this is comment added
+ *
  * @author Ronak
  */
-public class Student {
-       private String name;
-       private String address;
+public class Student
+{
+   private String name;
+   private String address;
 
-    public String getAddress() {
-        return address;
-    }
+   public String getAddress ()
+   {
+      return address;
+   }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+   public void setAddress (String address)
+   {
+      this.address = address;
+   }
 
-    public String getName() {
-        return name;
-    }
+   public String getName ()
+   {
+      return name;
+   }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+   public void setName (String name)
+   {
+      this.name = name;
+   }
 
-    
-   
-    
+
+
+
 }
